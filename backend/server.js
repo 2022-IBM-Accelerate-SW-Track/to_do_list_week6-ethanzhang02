a3d6f150abbf3387323b1e6e7c8d4b0d422f28d8
Ethan Zhang <ethanzhang@macbook-air-5.myfiosgateway.com>
2022-08-01 14:53:57 -0400

    done

--- backend/server.js
+++ backend/server.js
@@ -69,3 +69,37 @@ app.get("/get/searchitem",searchItems)
     response.json(returnData);
     //Note this won't work, why? response.send();
   }
+
+  const basicAuth = require("express-basic-auth");
+  var { authenticator, upsertUser, cookieAuth } = require("./authentication");
+  const auth = basicAuth({
+      authorizer: authenticator
+  });
+  const cookieParser = require("cookie-parser");
+  app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
+  app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
+
+app.get("/authenticate", auth, (req, res) => {
+  console.log(`user logging in: ${req.auth.user}`);
+  res.cookie('user', req.auth.user, { signed: true });
+  res.sendStatus(200);
+});
+
+app.post("/users", (req, res) => {
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+  const upsertSucceeded = upsertUser(username, password)
+  res.sendStatus(upsertSucceeded ? 200 : 401);
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie('user');
+  res.end();
+});
+
+app.post("/add/item", cookieAuth, addItem);
+app.get("/get/items", cookieAuth, getItems);
+app.get("/get/searchitem", cookieAuth, searchItems);
